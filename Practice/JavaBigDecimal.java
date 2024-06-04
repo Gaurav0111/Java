ad30766f28787b380b0d22ffcf859898477a9678
Gaurav0111 <garwalgaurav1@gmail.com>
2024-06-04 21:54:29 +0530

    added pattern program

--- Practice/JavaBigDecimal.java
+++ Practice/JavaBigDecimal.java
@@ -5,18 +5,20 @@ public class JavaBigDecimal {
     public static void main(String[] args) {
         // Input
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String[] s = new String[n + 2];
-        for (int i = 0; i < n; i++) {
-            s[i] = sc.next();
-        }
-        sc.close();
+        // int n = sc.nextInt();
+        // String[] s = new String[n];
+        // for (int i = 0; i < n; i++) {
+        //     s[i] = sc.next();
+        // }
+        String str1 = "hey12";
+        String str2 = "hey";
+        System.out.println(str1.);
+        // if(str1.compareTo(str2)){
 
-        // Write your code here
-
-        // Output
-        for (int i = 0; i < n; i++) {
-            System.out.println(s[i]);
-        }
+        // }
+        // sc.close();
+        // for (int i = 0; i < n; i++) {
+            // System.out.println(s[i]);
+        // }
     }
 }
