af129fa88d04c922b83647bec4b320910f2152e4
Gaurav0111 <garwalgaurav1@gmail.com>
2024-06-08 21:47:45 +0530

    leetcode

--- Practice/Solution.java
+++ Practice/Solution.java
@@ -1,29 +1,20 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
-
-public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        String n = "2367495770217142995264827948666809233066409497699870112003149352380375124855230068487109373226251982";
-        try {
-            if (Integer.parseInt(n) <= 1) {
-                System.out.println("not prime");
-                System.exit(0);
-            }
-        } catch (Exception e) {
+class Solution {
+    public static int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 0) {
+            return 0;
         }
-        BigInteger bg = new BigInteger(n);
-        System.out.println(bg.isProbablePrime(10) ? "prime" : "not prime");
-        bufferedReader.close();
+        int left = 0, product = 0, count = 0;
+        for (int right = 0; right < nums.length; right++) {
+            product *= nums[right];
+            while (product >= k) {
+                product = product / nums[left];
+                left++;
+            }
+            count = count + right - left + 1;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        numSubarrayProductLessThanK(null, 0);
     }
 }
