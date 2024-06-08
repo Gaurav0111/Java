decd5fc79549cab42ff6fd09328fa47c80b52a7e
Gaurav0111 <garwalgaurav1@gmail.com>
2024-06-08 23:37:03 +0530

    minor

--- Practice/Solution.java
+++ Practice/Solution.java
@@ -1,20 +1,25 @@
-class Solution {
-    public static int numSubarrayProductLessThanK(int[] nums, int k) {
-        if (k <= 0) {
-            return 0;
-        }
-        int left = 0, product = 0, count = 0;
-        for (int right = 0; right < nums.length; right++) {
-            product *= nums[right];
-            while (product >= k) {
-                product = product / nums[left];
-                left++;
-            }
-            count = count + right - left + 1;
-        }
-        return count;
-    }
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
     public static void main(String[] args) {
-        numSubarrayProductLessThanK(null, 0);
+
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        String arr[] = scanner.nextLine().split(" ");
+        int left = 0, right = 0, count = 0;
+        ArrayList<ArrayList<String>> list = new ArrayList<>();
+        for (; left < n; left++) {
+            ArrayList<String> innerList2 = new ArrayList<>();
+            for (; right < n; right++) {
+                innerList2.add(Array.cop    );
+            }
+            list.add(innerList2);
+        }
+        for(String s : arr){
+            System.out.println(s);
+        }
+        System.out.println(list);
     }
 }
