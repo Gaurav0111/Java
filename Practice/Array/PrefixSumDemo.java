a900f71c1bdfd67a7e8fc6f1a429cd77792d73e0
Gaurav0111 <garwalgaurav1@gmail.com>
2024-06-11 16:31:46 +0530

    minor

---
+++ Practice/Array/PrefixSumDemo.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class PrefixSumDemo {
+    public static void main(String[] args) {
+        int org[] = { 1, 6, 3, 2, 7, 2 };
+        int quries[][] = { { 0, 3 }, { 2, 4 },{2,5} };
+        int prefix[] = new int[org.length];
+        prefix[0] = org[0];
+        for (int i = 1; i < org.length; i++) {
+            prefix[i] = prefix[i - 1] + org[i];
+        }
+        for (int i : prefix) {
+            System.out.println(i);
+        }
+
+        for (int i = 0; i < quries.length; i++) {
+            int lowerbound = quries[i][0];
+            int uppperBound = quries[i][1];
+            int sum = prefix[uppperBound] - prefix[lowerbound-1];
+            System.out.println("Sum: " + sum);
+        }
+    }
+}
---
+++ Practice/Array/TwoSum.java
@@ -0,0 +1,53 @@
+package Array;
+
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] arr, int target) {
+        int pair[] = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            int val = target - arr[i];
+            if (map.get(val) != null) {
+                pair[0] = map.get(val);
+                pair[1] = i;
+            }
+            map.put(arr[i], i);
+        }
+        return pair;
+    }
+}
+
+// class Solution {
+// public int[] twoSum(int[] nums, int target) {
+// int pair[] = new int[2];
+// int cpy[] = Arrays.copyOf(nums, nums.length);
+// Arrays.sort(cpy);
+// int i = 0, j = nums.length - 1;
+// while (i < j) {
+// int sum = cpy[i] + cpy[j];
+// if (sum == target) {
+// break;
+// } else if (sum < target) {
+// i++;
+// } else if (sum > target) {
+// j--;
+// }
+// }
+// for (int k = 0; k < nums.length; k++) {
+// if (nums[k] == cpy[i]) {
+// pair[0] = k;
+// }
+// }
+// for (int k = nums.length - 1; k >= 0; k--) {
+// if (nums[k] == cpy[j]) {
+// pair[1] = k;
+// }
+// }
+// return pair;
+// }
+// }
+
+public class TwoSum {
+
+}
