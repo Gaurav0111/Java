74218a4d96b42d1c3d9603f75301a527179911af
Gaurav0111 <garwalgaurav1@gmail.com>
2024-06-24 23:01:01 +0530

    gfg questions

---
+++ Practice/Array/KElementOfTwoSortedArray.java
@@ -0,0 +1,26 @@
+package Array;
+
+public class KElementOfTwoSortedArray {
+    static public long kthElement(int arr1[], int arr2[], int n, int m, int k) {
+        long result[] = new long[n + m];
+        int i = 0;
+        int j = 0;
+        int index = 0;
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                result[index] = arr1[i];
+                i++;
+                index++;
+            } else {
+                result[index] = arr2[j];
+                j++;
+                index++;
+            }
+        }
+        return result[k+1];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- Practice/TwoDArray/TransposeMatrix.java
+++ Practice/TwoDArray/TransposeMatrix.java
@@ -1,18 +1,47 @@
 package TwoDArray;
-import java.util.*;
+
+import java.util.ArrayList;
+
 public class TransposeMatrix {
 
     public int[][] transpose(int[][] matrix) {
         int result[][] = new int[matrix[0].length][matrix.length];
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                result[i][j] = matrix[j][i];
+            for (int j = 0; j < matrix[i].length; j++) {
+                result[j][i] = matrix[i][j];
             }
         }
         return result;
     }
 
-    public static void main(String[] args) {
+    static long maximumSumSubarray(int k, ArrayList<Integer> arr, int n) {
+        int result = 0;
+        int num = 0;
+        int first = 0;
+        int size = 0;
+        arr.
+        if (n < k)
+            return -1;
+        for (int end = 0; end < n; end++) {
+            num += arr.get(end);
+            size++;
+            while (size > k) {
+                num -= arr.get(first);
+                first++;
+                size--;
+            }
+            result = Math.max(result, num);
+        }
+        return result;
+    }
 
+    public static void main(String[] args) {
+        int n = 4, k = 2;
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(400);
+        list.add(200);
+        list.add(300);
+        list.add(100);
+        System.out.println(maximumSumSubarray(k, list, n));
     }
 }
