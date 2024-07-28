90b31613cbb9b713877665d72b11a16569d6830f
Gaurav0111 <garwalgaurav1@gmail.com>
2024-07-28 22:29:23 +0530

    String problem

---
+++ Practice/String/IndexOfFristOccurence.java
@@ -0,0 +1,11 @@
+package String;
+
+public class IndexOfFristOccurence {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
