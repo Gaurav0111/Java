5035469813500a39f0dac65571b647b0345dcb15
Gaurav0111 <garwalgaurav1@gmail.com>
2024-08-05 21:57:28 +0530

    calculating depth of binary tree

---
+++ DS/Tree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        
+    }
+    // public int maxDepth(TreeNode root) {
+    //     // int result = helper(root, 0);
+    //     if (root == null)
+    //         return 0;
+    //     int left = maxDepth(root.left);
+    //     int right = maxDepth(root.right);
+    //     int result = Math.max(left, right);
+    //     return result + 1;
+    // }
+}
+
+public class MaximumDepthOfBinaryTree {
+
+}
