b72aa58875cd9d529e98e2cd299b0bae1b0492cd
Gaurav0111 <garwalgaurav1@gmail.com>
2024-08-09 22:31:03 +0530

    minor

---
+++ Practice/String/ZigZagConversion.java
@@ -0,0 +1,19 @@
+package String;
+
+public class ZigZagConversion {
+    public static String convert(String s, int numRows) {
+        String result = "";
+        int index = 0;
+        for (int i = 0; i < s.length(); i++) {
+            result += s.charAt(index);
+            index = (index + numRows + 1) % s.length();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "PAYPALISHIRING";
+        System.out.println(convert(s, 3));
+    }
+}
+// PAHNAPLSIIGYIR
