05fb03e7c71cf5eb0dae5d22ecc4306a43b7b669
Gaurav0111 <garwalgaurav1@gmail.com>
2024-08-17 22:08:42 +0530

    leetcode questions

---
+++ Practice/String/MultiplyString.java
@@ -0,0 +1,15 @@
+package String;
+
+import java.math.BigInteger;
+
+public class MultiplyString {
+    public static String multiply(String num1, String num2) {
+        BigInteger num_1 = new BigInteger(num1);
+        BigInteger num_2 = new BigInteger(num2);
+        return ""+ num_1.multiply(num_2) ;
+        Integer.parseInt(num2)   
+    }
+    public static void main(String[] args) {
+        System.out.println(multiply("123456789", "654987321"));
+    }
+}
