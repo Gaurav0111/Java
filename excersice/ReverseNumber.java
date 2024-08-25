8390a60c187de59ef4ca8e46aa4bc31a7c80da82
Gaurav0111 <garwalgaurav1@gmail.com>
2024-08-25 22:24:02 +0530

    minor

--- excersice/ReverseNumber.java
+++ excersice/ReverseNumber.java
@@ -2,11 +2,14 @@ package excersice;
 
 public class ReverseNumber {
     public static void main(String[] args) {
-        int num =123,result=0;
-        while(num!=0){
-            int n = num%10;
-            result = (result*10)+n;
+        int num = 1534236469, result = 0;
+        int nn = 1534236469;
+        // int reverse = 9646324351;
+        while (num != 0) {
+            int n = num % 10;
+            result = (result * 10) + n;
             num /= 10;
+            // System.out.println();
         }
         System.err.println(result);
     }
