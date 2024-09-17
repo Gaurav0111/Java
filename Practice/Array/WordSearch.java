266a5a3a5d3826c047de0dfe19ebc3d3b991655c
Gaurav0111 <garwalgaurav1@gmail.com>
2024-09-17 23:59:14 +0530

    minor

--- Practice/Array/WordSearch.java
+++ Practice/Array/WordSearch.java
@@ -1,7 +1,13 @@
 package Array;
 
 public class WordSearch {
-    public static void main(String[] args) {
+    public static boolean exist(String[][] board, String word) {
         
+    }
+
+    public static void main(String[] args) {
+        String[][] board = { { "A", "B", "C", "E" }, { "S", "F", "C", "S" }, { "A", "D", "E", "E" } };
+        String word = "ABCCED";
+        System.out.println(exist(board, word));
     }
 }
