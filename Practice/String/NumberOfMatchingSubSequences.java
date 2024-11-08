aa21477e9a8249a13d7ae7ae83099677ea8c5498
Gaurav0111 <garwalgaurav1@gmail.com>
2024-11-08 22:58:36 +0530

    added subsequence function

--- Practice/String/NumberOfMatchingSubSequences.java
+++ Practice/String/NumberOfMatchingSubSequences.java
@@ -1,13 +1,32 @@
 package Practice.String;
 
 public class NumberOfMatchingSubSequences {
+
+    public static boolean helper(String str, String sub) {
+        int i = 0, j = 0;
+        while (i < str.length() && j < sub.length()) {
+            if (str.charAt(i) == sub.charAt(j)) {
+                j++;
+            }
+            i++;
+        }
+        return j == sub.length();
+    }
+
     public static int numMatchingSubseq(String s, String[] words) {
-        return 8;
+        int count = 0;
+        for (String str : words) {
+            if (helper(s, str)) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
     public static void main(String[] args) {
         String s = "abcde";
-        String[] words = { "a", "bb", "acd", "ace" };
+        String[] words = {"a", "bb", "acd", "ace"};
         System.out.println(numMatchingSubseq(s, words));
     }
 }
